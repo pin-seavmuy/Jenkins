9a87cd8fad9a3f85492f9ec638394a073caad672
Seavmuy <pinseavmouy99@gmail.com>
2024-03-29 16:50:52 +0700

    build project

---
+++ src/main/java/B/example/gradlee/GradleeApplication.java
@@ -0,0 +1,13 @@
+package B.example.gradlee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/B/example/gradlee/GradleeApplicationTests.java
@@ -0,0 +1,13 @@
+package B.example.gradlee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
